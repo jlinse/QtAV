34465d7f1b02f61bf958fd89686274bd5e4320b3
James Linse <jlinse@gearzy.com>
2017-08-16 18:48:02 -0600

    Updates to build.

--- src/AudioResamplerTemplate.cpp
+++ src/AudioResamplerTemplate.cpp
@@ -18,7 +18,7 @@
     License along with this library; if not, write to the Free Software
     Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301  USA
 ******************************************************************************/
-
+#define BUILD_SWR
 #if defined(BUILD_AVR) || defined(BUILD_SWR) // no this macro is fine too for qmake
 #include "QtAV/AudioResampler.h"
 #include "QtAV/private/AudioResampler_p.h"
